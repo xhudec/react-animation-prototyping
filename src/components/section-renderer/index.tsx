4db38071b2b7d799269074fe4142862ecccdb2e0
Lukas Hudec <hudec.luke@gmail.com>
2021-06-14 16:40:56 +0200

    fix: failing build

--- src/components/section-renderer/index.tsx
+++ src/components/section-renderer/index.tsx
@@ -17,47 +17,53 @@ export interface ISectionRendererProps {
 }
 
 export default function SectionRenderer({ templates }: ISectionRendererProps) {
-  return templates.map((template) => {
-    switch (template.type) {
-      case 'counter-and-six-multimedia-layout': {
-        const [firstColumn, secondColumn] = template.multimedia.columns
+  return (
+    <>
+      {templates.map((template) => {
+        switch (template.type) {
+          case 'counter-and-six-multimedia-layout': {
+            const [firstColumn, secondColumn] = template.multimedia.columns
 
-        return (
-          <DemoSection data-scroll-section>
-            <SectionTextContent data-scroll-speed="0.5">
-              <AnimatedCounter
-                data-scroll
-                data-scroll-id={`${template.type}-${template.count}`}
-                from={template.count * 0.75}
-                to={template.count}
-              />
-              <Heading2 data-scroll>{template.title}</Heading2>
-            </SectionTextContent>
-            <DemoColumns>
-              <Column data-scroll data-scroll-speed="16">
-                {firstColumn.map((block, index) => (
-                  <BlockRenderer key={index} block={block} />
-                ))}
-              </Column>
-              <Column data-scroll data-scroll-speed="8">
-                {secondColumn.map((block, index) => (
-                  <BlockRenderer key={index} block={block} />
-                ))}
-              </Column>
-            </DemoColumns>
-          </DemoSection>
-        )
-      }
-      case 'center-text': {
-        return (
-          <CenterTextSection data-scroll-section>
-            <Heading2 data-scroll data-scroll-speed="2">{template.title}</Heading2>
-            <Paragraph data-scroll>{template.content}</Paragraph>
-          </CenterTextSection>
-        )
-      }
-      default:
-        return null
-    }
-  })
+            return (
+              <DemoSection data-scroll-section>
+                <SectionTextContent data-scroll-speed="0.5">
+                  <AnimatedCounter
+                    data-scroll
+                    data-scroll-id={`${template.type}-${template.count}`}
+                    from={template.count * 0.75}
+                    to={template.count}
+                  />
+                  <Heading2 data-scroll>{template.title}</Heading2>
+                </SectionTextContent>
+                <DemoColumns>
+                  <Column data-scroll data-scroll-speed="16">
+                    {firstColumn.map((block, index) => (
+                      <BlockRenderer key={index} block={block} />
+                    ))}
+                  </Column>
+                  <Column data-scroll data-scroll-speed="8">
+                    {secondColumn.map((block, index) => (
+                      <BlockRenderer key={index} block={block} />
+                    ))}
+                  </Column>
+                </DemoColumns>
+              </DemoSection>
+            )
+          }
+          case 'center-text': {
+            return (
+              <CenterTextSection data-scroll-section>
+                <Heading2 data-scroll data-scroll-speed="2">
+                  {template.title}
+                </Heading2>
+                <Paragraph data-scroll>{template.content}</Paragraph>
+              </CenterTextSection>
+            )
+          }
+          default:
+            return null
+        }
+      })}
+    </>
+  )
 }
